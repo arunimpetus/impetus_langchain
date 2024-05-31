c75669eec765e37e268e6c87ce5dabd32d360f0c
Arun Kumar <arun.kumar@impetus.com>
2024-05-31 23:38:57 +0530

    unit case added

--- libs/community/tests/unit_tests/document_loaders/test_kyvos_loader.py
+++ libs/community/tests/unit_tests/document_loaders/test_kyvos_loader.py
@@ -2,110 +2,42 @@ from pathlib import Path
 from langchain_core.documents import Document
 from langchain_community.document_loaders.kyvos_loader import KyvosLoader
 
+import os
+
 
 class TestKyvosLoader:
+    parsed_json = {'login_url': 'https://trial.kyvosinsights.com/kyvos/rest/login',
+                   'query_url': 'https://trial.kyvosinsights.com/kyvos/rest/export/query',
+                   'query_type': 'SQL',
+                   'output_format': 'csv',
+                   'line_seperator': '%5Cr%5Cn',
+                   'enclosed_by': "'",
+                   'connector_type': 'Rest',
+                   'zipped': 'false',
+                   'include_header': 'true',
+                   'kms': 'false',
+                   'output_file_name': "sample_data_1.csv",
+                   'header_accept': 'application/octet-stream',
+                   'maxRows': 1000000}
+    limit = 2
+    username = 'trialuser1'
+    password = 'Welcome@123'
+    query = f"SELECT `a ssb technical performance 30b_v2`.`customer id` AS `customer id`, `a ssb technical performance 30b_v2`.`postal code` AS `postal code`, `a ssb technical performance 30b_v2`.`city` AS `city`, `a ssb technical performance 30b_v2`.`country` AS `country` FROM `ssb - manufacturing use case`.`a ssb technical performance 30b_v2` `a ssb technical performance 30b_v2` GROUP BY  `a ssb technical performance 30b_v2`.`customer id`, `a ssb technical performance 30b_v2`.`postal code`, `a ssb technical performance 30b_v2`.`city`, `a ssb technical performance 30b_v2`.`country` LIMIT {limit}"
+
     # Tests that a CSV file with valid data is loaded successfully.
     def test_kyvos_loader_load_valid_data(self) -> None:
         # Setup
-        file_path = self._get_csv_file_path("test_nominal.csv")
-        expected_docs = [
-            Document(
-                page_content="column1: value1\ncolumn2: value2\ncolumn3: value3",
-                metadata={"source": file_path, "row": 0},
-            ),
-            Document(
-                page_content="column1: value4\ncolumn2: value5\ncolumn3: value6",
-                metadata={"source": file_path, "row": 1},
-            ),
-        ]
+        expected_docs = [Document(page_content="'customer id': '18247','postal code': '380001','city': 'Ahmedabad',"
+                                               "'country': 'India'", metadata={'file_name': 'sample_data_1.csv',
+                                                                               'row_no': 0}),
+                         Document(
+                             page_content="'customer id': '24010','postal code': '380001','city': 'Ahmedabad',"
+                                          "'country': 'India'",
+                             metadata={'file_name': 'sample_data_1.csv', 'row_no': 1})]
 
         # Exercise
-        loader = CSVLoader(file_path=file_path)
+        loader = KyvosLoader(configuration_parameters=parsed_json, query=query, username=username, password=password)
         result = loader.load()
 
         # Assert
         assert result == expected_docs
-
-    # Tests that an empty CSV file is handled correctly.
-    def test_csv_loader_load_empty_file(self) -> None:
-        # Setup
-        file_path = self._get_csv_file_path("test_empty.csv")
-        expected_docs: list = []
-
-        # Exercise
-        loader = CSVLoader(file_path=file_path)
-        result = loader.load()
-
-        # Assert
-        assert result == expected_docs
-
-    # Tests that a CSV file with only one row is handled correctly.
-    def test_csv_loader_load_single_row_file(self) -> None:
-        # Setup
-        file_path = self._get_csv_file_path("test_one_row.csv")
-        expected_docs = [
-            Document(
-                page_content="column1: value1\ncolumn2: value2\ncolumn3: value3",
-                metadata={"source": file_path, "row": 0},
-            )
-        ]
-
-        # Exercise
-        loader = CSVLoader(file_path=file_path)
-        result = loader.load()
-
-        # Assert
-        assert result == expected_docs
-
-    # Tests that a CSV file with only one column is handled correctly.
-    def test_csv_loader_load_single_column_file(self) -> None:
-        # Setup
-        file_path = self._get_csv_file_path("test_one_col.csv")
-        expected_docs = [
-            Document(
-                page_content="column1: value1",
-                metadata={"source": file_path, "row": 0},
-            ),
-            Document(
-                page_content="column1: value2",
-                metadata={"source": file_path, "row": 1},
-            ),
-            Document(
-                page_content="column1: value3",
-                metadata={"source": file_path, "row": 2},
-            ),
-        ]
-
-        # Exercise
-        loader = CSVLoader(file_path=file_path)
-        result = loader.load()
-
-        # Assert
-        assert result == expected_docs
-
-    def test_csv_loader_load_none_column_file(self) -> None:
-        # Setup
-        file_path = self._get_csv_file_path("test_none_col.csv")
-        expected_docs = [
-            Document(
-                page_content="column1: value1\ncolumn2: value2\n"
-                "column3: value3\nNone: value4,value5",
-                metadata={"source": file_path, "row": 0},
-            ),
-            Document(
-                page_content="column1: value6\ncolumn2: value7\n"
-                "column3: value8\nNone: value9",
-                metadata={"source": file_path, "row": 1},
-            ),
-        ]
-
-        # Exercise
-        loader = CSVLoader(file_path=file_path)
-        result = loader.load()
-
-        # Assert
-        assert result == expected_docs
-
-    # utility functions
-    def _get_csv_file_path(self, file_name: str) -> str:
-        return str(Path(__file__).resolve().parent / "test_docs" / "csv" / file_name)
